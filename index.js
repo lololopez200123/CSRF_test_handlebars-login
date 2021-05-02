e75ee1c00206ff532d23bcbf4870543a0d5347c3
lololopez200123 <57774749+lololopez200123@users.noreply.github.com>
2021-05-02 08:51:16 -0300

    add dependencies require const to index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 /* eslint-disable indent */
 /* eslint-disable padded-blocks */
 /* eslint-disable quotes */
+
+const express = require("express");
+const fs = require("fs");
+const handlebars = require("express-handlebars");
